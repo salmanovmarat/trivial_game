58337807fe6c948f22f7fc612d5739795ba297c6
Your Name <you@example.com>
2019-04-08 18:28:38 +0400

    commit

--- javascript.js
+++ javascript.js
@@ -1,9 +1,38 @@
+
+var array =[]
+var correct_answer
+
+
+
+
 $("#start").on("click", function(){
-    $("button").remove("#start")
-    quest()
+    $("button").remove()
+    question()
 })
-function quest(){
-    var newDiv= $("<div>")
-    newDiv.htm("testing")
-    $("#trivia").append(newDiv)
-}
+function question(){
+    $.ajax({
+        url:"https://opentdb.com/api.php?amount=8&difficulty=easy&type=multiple"
+    }).done(function(response){
+        console.log(response)
+        var quest1 = response.results[0].question
+       var newDiv = $("<div class=question>").html(quest1)
+       $("#trivia-btn").append(newDiv)
+       for( var i=0; i<response.results.length; i++){
+        console.log(response[i])
+        var answ = response.results[0].incorrect_answers
+    }
+       
+      
+       
+       array.push(answ)
+       var duzgun = response.results[0].correct_answer
+       array[0].push(duzgun)
+       
+       var div = $("<div class = answer>").html(answ)
+       $("#trivia-btn").append(div)
+
+       console.log(array)
+        
+       
+    })
+    }
