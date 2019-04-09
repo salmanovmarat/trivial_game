d8b2993794aadea875c342ced571d82602865fef
Your Name <you@example.com>
2019-04-09 18:04:44 +0400

    again

--- javascript.js
+++ javascript.js
@@ -1,38 +1,35 @@
 
-var array =[]
-var correct_answer
+var questions = [{
+  question: "1 . What does http stand for?",
+  answers: ["Hypertext Terminal Protocol", "Hypertext Transfer Protocol", "Hypertext Transmission Protocol", "HypeText Trans Protocol"],
+  correctAnswer: "Hypertext Transfer Protocol"
+}, {
+  question: "2 . What is the common shortcut for the ''paste'' function?",
+  answers: ["ctrl + p", "ctrl + s", "ctrl + v", "ctrl + c"],
+  correctAnswer: "ctrl + v"
+}, {
+  question: "3 . Which of the following is a video file format?",
+  answers: [".png", ".avi", ".bmp", ".mp3"],
+  correctAnswer: ".avi"
+}, {
+  question: "4 . Which web browser is developed by Apple?",
+  answers: ["Chrome", "Opera", "Safari", "Firefox"],
+  correctAnswer: "Safari"
+}, {
+  question: "5 . Which of the following is the smallest unit of memory size?",
+  answers: ["Terabyte", "Gigabyte", "Kilobyte", "Megabyte"],
+  correctAnswer: "Kilobyte"
+}, {
+  question:  "6 . Which of the following is not an Operating System?",
+  answers: ["Windows", "Linux", "OS X", "Java"],
+  correctAnswer: "Java"
+}, {
+  question: "7 . What does the computing term ''RAM'', stand for?",
+  answers: ["Random Access Memory", "Reliable Alternate Memory", "Reverse Architecture Memory", "Reliable Access Memory"],
+  correctAnswer: "Random Access Memory"
+}, {
+  question: "8 . How many computer languages are in use??",
+  answers: ["20", "2000", "50", "4500"],
+  correctAnswer: "2000"
+}];
 
-
-
-
-$("#start").on("click", function(){
-    $("button").remove()
-    question()
-})
-function question(){
-    $.ajax({
-        url:"https://opentdb.com/api.php?amount=8&difficulty=easy&type=multiple"
-    }).done(function(response){
-        console.log(response)
-        var quest1 = response.results[0].question
-       var newDiv = $("<div class=question>").html(quest1)
-       $("#trivia-btn").append(newDiv)
-       for( var i=0; i<response.results.length; i++){
-        console.log(response[i])
-        var answ = response.results[0].incorrect_answers
-    }
-       
-      
-       
-       array.push(answ)
-       var duzgun = response.results[0].correct_answer
-       array[0].push(duzgun)
-       
-       var div = $("<div class = answer>").html(answ)
-       $("#trivia-btn").append(div)
-
-       console.log(array)
-        
-       
-    })
-    }
